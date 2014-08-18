781b01b691cfa9112a492adfeada9a0c9b550c80
Bruno Dias <brunoald@gmail.com>
2014-08-18 13:43:00 +0100

    Fix reference to non-existing task "scripts".

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('imagemin', function () {
 gulp.task('watch', function () {
   gulp.watch(['sass/**/*.scss', 'sass/**/*.sass'],['styles']);
   gulp.watch('jade/**/*.jade',['templates']);
-  gulp.watch('scripts/**/*.js',['scripts']);
+  gulp.watch('scripts/**/*.js',['js', 'js-concat']);
   gulp.watch(['images/**/*.svg', 'images/**/*.png', 'images/**/*.jpg', 'images/**/*.gif'],['imagemin']);
 });
 
